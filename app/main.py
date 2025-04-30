99a3ade2d730d7cddd671669f44893ffc56f48cf
Võ Tường Long <volong2004@gmail.com>
2025-04-30 17:05:34 +0700

    up

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from .database import engine, Base
 from .routes import movie_routes
+from controllers.movie_controller import get_movie 
 
 # Tạo bảng nếu chưa có
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
+get_movie()
 # Đăng ký router
 app.include_router(movie_routes.router, prefix="/api", tags=["Movies"])
 @app.get("/")
