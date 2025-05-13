a40f13ea0575cff7ae8ace91a1492138c228ea4d
Võ Tường Long <volong2004@gmail.com>
2025-05-13 22:28:48 +0700

    up

--- myanimelist/build/lib/myanimelist/pipelines.py
+++ myanimelist/build/lib/myanimelist/pipelines.py
@@ -26,18 +26,37 @@ class CustomImagePipeline(ImagesPipeline):
                     # Handle character images
                     for charater_name, charater_link in image_url.items():
                         if charater_name and charater_link["character"]:
-                            yield Request(charater_link["character"], meta={"anime_id": item["id"], "image_type": image_type, "character_name": charater_name, "character_type": "character"})
+                            yield Request(
+                                charater_link["character"],
+                                meta={
+                                    "anime_id": item["id"],
+                                    "image_type": image_type,
+                                    "character_name": charater_name,
+                                    "character_type": "character",
+                                },
+                            )
                         if charater_name and charater_link["voice_actor"]:
-                            yield Request(charater_link["voice_actor"], meta={"anime_id": item["id"], "image_type": image_type, "character_name": charater_name, "character_type": "voice_actor"})
+                            yield Request(
+                                charater_link["voice_actor"],
+                                meta={
+                                    "anime_id": item["id"],
+                                    "image_type": image_type,
+                                    "character_name": charater_name,
+                                    "character_type": "voice_actor",
+                                },
+                            )
                 else:
 
-                    yield Request(image_url, meta={"anime_id": item["id"], "image_type": image_type})
+                    yield Request(
+                        image_url,
+                        meta={"anime_id": item["id"], "image_type": image_type},
+                    )
 
     def file_path(self, request, response=None, info=None, *, item=None):
         # Use the anime ID and image type as the filename
         anime_id = request.meta.get("anime_id", "unknown")
         image_type = request.meta.get("image_type", "unknown")
-        url_without_query = request.url.split('?')[0]
+        url_without_query = request.url.split("?")[0]
         image_extension = os.path.splitext(url_without_query)[1]
 
         if image_type == "characters":
@@ -45,8 +64,11 @@ class CustomImagePipeline(ImagesPipeline):
             character_type = request.meta.get("character_type", "unknown")
 
             # Ensure character_name is sanitized
-            sanitized_name = re.sub(
-                r'[^\w\-_]', '_', self.character_name) if self.character_name else "unknown_character"
+            sanitized_name = (
+                re.sub(r"[^\w\-_]", "_", self.character_name)
+                if self.character_name
+                else "unknown_character"
+            )
 
             if character_type == "character":
                 # For character images
@@ -56,7 +78,9 @@ class CustomImagePipeline(ImagesPipeline):
                 return f"{image_type}_{anime_id}_{sanitized_name}_voiceActor{image_extension}"
             else:
                 # Handle unexpected character types
-                return f"{image_type}_{anime_id}_{sanitized_name}_unknown{image_extension}"
+                return (
+                    f"{image_type}_{anime_id}_{sanitized_name}_unknown{image_extension}"
+                )
 
         # Default filename for other image types
         return f"{image_type}_{anime_id}{image_extension}"
